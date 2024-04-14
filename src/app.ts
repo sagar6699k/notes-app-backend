69246bd7055d984245090a8a178223d1bdea7020
Sagar <sagarkurewar@gmail.com>
2024-04-14 17:49:59 +0530

    adding backend dir

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,6 @@ import cookieParser from "cookie-parser";
 import 'express-async-errors'
 import api from "./api/index.api";
 import noteRouter from "../src/routes/note.route"
-
 config();
 
 const app = express();
