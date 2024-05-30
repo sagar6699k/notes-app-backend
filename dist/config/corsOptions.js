489b530169aa20d93e68675aa520a20c83043344
Sagar <sagarkurewar@gmail.com>
2024-05-30 17:12:26 +0530

    updated code with correction of error

--- dist/config/corsOptions.js
+++ dist/config/corsOptions.js
@@ -4,17 +4,25 @@ exports.corsOptions = void 0;
 const allowedOrigins = [
     "http://localhost:3000",
     "http://192.168.1.2:3000",
-    "https://notes-app-frontend-sigma.vercel.app/"
+    "https://notes-app-frontend-sigma.vercel.app/",
 ];
+// export const corsOptions: cors.CorsOptions = {
+//   origin: (
+//     origin: string | undefined,
+//     callback: (error: Error | null, allow?: boolean) => void
+//   ) => {
+//     if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
 exports.corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        }
-        else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: true, // This allows all origins
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
     credentials: true,
     optionsSuccessStatus: 200,
